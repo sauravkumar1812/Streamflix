95cecd320f7b47bd12e7d867940aa165f2e9ed45
saurav1812 <shorya181299@gmail.com>
2023-07-31 23:25:13 +0530

    normal commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
+import { useState } from "react";
 import "./App.css";
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name :{props.name} </h1>
-      <h1>Last Name : {props.lastname} </h1>
-      <h2>Age : {props.age}</h2>
-    </>
-  );
-};
 
 const App = () => {
   return (
     <div className="App">
-      <Person name={"VISHAL"} lastname={"YADAV"} age={21} />
-      <Person name={"SAURAV"} lastname={"KUMAR"} age={23} />
-      <Person name={"AYUSH"} lastname={"SINGH"} age={22} />
+    
     </div>
   );
 };

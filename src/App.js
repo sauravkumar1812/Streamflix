dfa69bcb69db54cf61862c330b42232faf5e8a72
saurav1812 <shorya181299@gmail.com>
2023-07-31 23:32:27 +0530

    normal commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const  [counter,setCounter] = useState(0);
   return (
     <div className="App">
-    
+       <button onClick={()=>setCounter((prevCount)=>)}>-</button>
+       <h1>
+        {counter}
+       </h1>
+       <button onClick={()=>alert('clicked')}>+</button>
     </div>
   );
 };

84071b67ba34e320f4df872d61f47ceb158d2733
saurav1812 <shorya181299@gmail.com>
2023-07-31 23:21:05 +0530

    learning commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-const Person = () =>{
+const Person = (props) =>{
   return(
     <>
-  <h1>Name : 'SAURAV'</h1>
-  <h1>Last Name : 'Kuamr' </h1>
-  <h2>Age : 22</h2>
+  <h1>Name :{props.name}  </h1>
+  <h1>Last Name : {props.lastname} </h1>
+  <h2>Age : {props.age}</h2>
   </>
   )
 } 
@@ -12,7 +12,9 @@ const Person = () =>{
 const App = () => {
   return (
     <div className="App">
-   <h1>Hello, {9+9} !</h1> 
+  <Person name= {'VISHAL'}lastname= {'YADAV'} age={21}/>
+  <Person name = {'SAURAV'} lastname={'KUMAR' } age = {23}/>
+  <Person name = {'AYUSH'} lastname ={'SINGH'} age={22}/>
     </div>
   );
 }

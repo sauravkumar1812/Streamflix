28370ae2427b36078ba314bfdbbc11f0aba81313
saurav1812 <shorya181299@gmail.com>
2023-08-02 00:07:23 +0530

    normal commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import { useEffect } from "react";
 // 9682e42a
-import './App.css';
-import SearchIcon from './search.svg';
-const API_URL ="http://www.omdbapi.com?apikey=9682e42a";
-
-const App = () => {
-
-  const searchMovies = async (title)=>{
-   const response =await fetch(`${API_URL}&s=${title}`);
-   const data  =await response.json();
-
-   console.log(data.Search);
-  }
-  useEffect ( () => {
-    searchMovies('Spiderman');
-  },[]);
-  return(
-    <div className="app">
-      <h1>StreamFlix</h1>
-    </div>
-  );
+import "./App.css";
+import SearchIcon from "./search.svg";
+const API_URL = "http://www.omdbapi.com?apikey=9682e42a";
+const movie1 ={
+    "Title": "Italian Spiderman",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
 }
 
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+  return (
+    <div className="app">
+      <h1>StreamFlix</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img
+        src={SearchIcon} 
+        alt="search"
+        onClick={()=>{}}
+        />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+            src={movie1.Poster !== 'N/A '? movie1.Poster:'htpps://via.placeholder.com/400'}
+             alt="movie1.title"
+            />
+          </div>
+          <div>
+            <span>
+              {movie1.Type}
+            </span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default App;
-  

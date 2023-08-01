1cbe32e72d8030daba88faad989299d4f8338ca9
saurav1812 <shorya181299@gmail.com>
2023-08-01 23:17:52 +0530

    normal commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
 // 9682e42a
 const API_URL ='http://www.omdbapi.com?apikey= 9682e42a';
 
 const App = () => {
+  const searchMovies = async (title)=>{
+   const response =await fetch(`${API_URL}&s=${title}`)
+  }
+  useEffect ( () => {
+    
+  },[]);
   return(
     <h1>HII</h1> 
   );

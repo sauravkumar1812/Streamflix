a9fd889806a24b1b39b9ca62026065b578fff8cf
saurav1812 <shorya181299@gmail.com>
2023-08-01 23:32:44 +0530

    normal commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
 // 9682e42a
-const API_URL ='http://www.omdbapi.com?apikey= 9682e42a';
+const API_URL ="http://www.omdbapi.com?apikey=9682e42a";
 
 const App = () => {
+
   const searchMovies = async (title)=>{
-   const response =await fetch(`${API_URL}&s=${title}`)
+   const response =await fetch(`${API_URL}&s=${title}`);
+   const data  =await response.json();
+
+   console.log(data.Search);
   }
   useEffect ( () => {
-    
+    searchMovies('Spiderman');
   },[]);
   return(
     <h1>HII</h1> 
